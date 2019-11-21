d4f54947def5cc0360e8e346fc22e514e39e63fa
Jim Weist <jp_weist@yahoo.com>
2019-11-21 16:15:05 -0700

    final fixes

--- test/Round-test.js
+++ test/Round-test.js
@@ -12,7 +12,7 @@ describe('Round', function() {
   beforeEach(() => {
     card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
 
-    card2 = new Card (2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+    card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
 
     card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
 
@@ -48,7 +48,6 @@ describe('Round', function() {
   });
   it('should have incorrect guesses will be stored (via the id) in an array of incorrectGuesses', function() {
     round.takeTurn(guessWrong, card1);
-    // console.log(round.incorrectGuesses[0])
     expect(round.incorrectGuesses[0]).to.equal(card2)
   });
   it('should have feedback is returned regarding whether the guess is incorrect or correct', function() {
